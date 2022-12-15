6414524001b82dfc4a4a0e1f394e0bd10846018b
PrateekRana1-2 <thakurprateek0001@gmail.com>
2022-12-15 18:09:20 +0530

    test case  update

--- src/test/java/com/estate/RealEstateApplicationTests.java
+++ src/test/java/com/estate/RealEstateApplicationTests.java
@@ -9,5 +9,13 @@ class RealEstateApplicationTests {
 	//@Test
 	//void contextLoads() {
 	//}
+	@Test
+	void findByid() {
+
+		CoustmerEntity entity=new CoustmerEntity(null, null, null, null, null, null, null, false, null, null, null);
+//		coustmerRepo.save(entity);
+		Boolean actualresult=coustmerRepo.existsById((long) 1);
+		assertThat(actualresult).isEqualTo(true);
+	}
 
 }
